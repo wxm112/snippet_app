05c86ad74d5b22311b90ff955089773eec73e918
王雪梅 <wxm112@me.com>
2015-02-09 19:26:02 +1100

    routes: removing unneeded json/show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'json/show'
 
   root :to => 'pages#home'
 
   resources :users, :only => [:new, :create, :edit, :show, :update,:index]
   resources :snippets
 
-  get '/snippets/json/:name' =>'json#show'
+  get '/snippets/json/:id' =>'json#show'
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
