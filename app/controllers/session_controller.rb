4e58309c8b362870fc20670f47a10931588336fd
王雪梅 <wxm112@me.com>
2015-02-11 20:04:24 +1100

    log in fixed

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,9 +4,15 @@ class SessionController < ApplicationController
 
   def create
     user = User.find_by :name => params[:username]
-    if user.present? && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to(user_path(user.id))
+    if 
+      user.present? && user.authenticate(params[:password]) 
+      if user.confirmed 
+        session[:user_id] = user.id
+        redirect_to(user_path(user.id))
+      else
+        flash[:error] = "Your account hasn't been actived. Please active your account by email "
+        redirect_to(login_path)
+      end
     else
       flash[:error] = "Invalid login or password"
       redirect_to(login_path)
